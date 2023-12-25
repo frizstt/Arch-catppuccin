2b6a6c2da86efe558ea336cb71acf5975658794d
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2023-12-25 15:25:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 ##################################################
 #INSTALL PACKAGES
 ##################################################
-sudo pacman -Sy --needed alacritty btop i3-wm i3lock neofetch neovim picom polybar zathura breeze-gtk firefox flameshot font-manager gnome-weather lxappearance nvidia obsidian pavucontrol polkit-gnome ranger redshift sddm thunar zsh zsh-syntax-highlighting ttf-font-awesome feh flatpak file-roller discord retroarch libretro-mupen64plus-next libretro-snes9x retroarch-assets-glui steam vlc wget gimp rnote flatpak zathura-pdf-poppler xorg-server xorg-xprop xorg-xrandr
+sudo pacman -Syy
+sudo pacman -Sy --no-confirm --needed alacritty btop i3-wm i3lock neofetch neovim picom polybar zathura breeze-gtk firefox flameshot font-manager gnome-weather lxappearance nvidia obsidian pavucontrol polkit-gnome ranger redshift sddm thunar zsh zsh-syntax-highlighting ttf-font-awesome feh flatpak file-roller discord retroarch libretro-mupen64plus-next libretro-snes9x retroarch-assets-glui steam vlc wget gimp rnote flatpak zathura-pdf-poppler xorg-server xorg-xprop xorg-xrandr
 sudo systemctl enable sddm.service
 
 clear
