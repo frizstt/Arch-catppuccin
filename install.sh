ea196f105b563264595f4626183f2c0d5dabe750
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2023-12-25 17:27:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,27 +28,6 @@ echo "##################################################"
 sleep 5
 clear
 
-
-##################################################
-#INSTALL FONTS
-##################################################
-
-font-manager -i Misc/Fonts/JetBrainsMonoNerdFont/JetBrainsMono_Nerd_Font_Bold.ttf
-font-manager -i Misc/Fonts/JetBrainsMonoNerdFont/JetBrainsMono_Nerd_Font_Bold_Italic.ttf
-font-manager -i Misc/Fonts/JetBrainsMonoNerdFont/JetBrainsMono_Nerd_Font_Italic.ttf
-font-manager -i Misc/Fonts/JetBrainsMonoNerdFont/JetBrainsMono_Nerd_Font_Regular.ttf
-font-manager -i Misc/Fonts/MesloLGSNF/MesloLGS_NF_Bold.ttf
-font-manager -i Misc/Fonts/MesloLGSNF/MesloLGS_NF_Bold_Italic.ttf
-font-manager -i Misc/Fonts/MesloLGSNF/MesloLGS_NF_Italic.ttf
-font-manager -i Misc/Fonts/MesloLGSNF/MesloLGS_NF_Regular.ttf
-
-clear
-echo "##################################################"
-echo "Installed all required fonts, continuing..."
-echo "##################################################"
-sleep 5
-clear
-
 ##################################################
 #INSTALL PACKER.NVIM
 ##################################################
