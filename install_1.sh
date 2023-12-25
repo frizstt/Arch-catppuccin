13021c90c7e3b1e5419d748c77847b5be4ccc345
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2023-12-25 16:24:50 +0200

    Update install_1.sh

--- install_1.sh
+++ install_1.sh
@@ -4,15 +4,15 @@
 #SET UP ZSH
 ##################################################
 clear
-echo"##################################################"
+echo "##################################################"
 echo 'Now, oh-my-zsh will be installed. When it asks you if you want it to be the default shell, say yes. Also after it installs, type "exit" to continue the installation.'
-echo"##################################################"
+echo "##################################################"
 sleep 10
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 source .zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-exec .zsh
+exec zsh
 mv Dots/zshrc Dots/.zshrc
 mv Dots/.zshrc ~/.zshrc
 source .zshrc
