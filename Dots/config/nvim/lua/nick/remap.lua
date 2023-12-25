7184d44793f712c7908ea36f07406e330b331cd4
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2023-12-25 15:07:25 +0200

    Add files via upload

---
+++ Dots/config/nvim/lua/nick/remap.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>xo", vim.cmd.NvimTreeOpen)
+vim.keymap.set("n", "<leader>xc", vim.cmd.NvimTreeClose)
+
