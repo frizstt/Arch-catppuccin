c9e517bc89fa445ada2ea6263c16a560e5909aa1
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2023-12-25 16:03:22 +0200

    Update install_1.sh

--- install_1.sh
+++ install_1.sh
@@ -3,6 +3,11 @@
 ##################################################
 #SET UP ZSH
 ##################################################
+clear
+echo"##################################################"
+echo 'Now, oh-my-zsh will be installed. When it asks you if you want it to be the default shell, say yes. Also after it installs, type "exit" to continue the installation.'
+echo"##################################################"
+sleep 10
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 source .zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
