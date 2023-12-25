b54235f3fc2c2802b0759e83786a5858260bcb69
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2023-12-25 15:08:05 +0200

    Add files via upload

---
+++ Misc/Scripts/powermanagement.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import os
+root = tk.Tk()
+
+
+root.configure(bg="#1e1e2e")
+
+
+def goToSleep():
+    os.system("~/.config/i3scripts/lock && systemctl suspend")
+
+
+def goToWake():
+    os.system("reboot")
+
+
+def goToDie():
+    os.system("shutdown -h now")
+
+
+sleep = tk.Button(text="Sleep", width=50, height=10, command=goToSleep)
+reboot = tk.Button(text="Reboot", width=50, height=10, command=goToWake)
+shutdown = tk.Button(text="Shutdown", width=50, height=10, command=goToDie)
+
+sleep.pack()
+reboot.pack()
+shutdown.pack()
+
+root.mainloop()
