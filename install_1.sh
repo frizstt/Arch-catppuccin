dd442ac77171970165c7ff3d38471e2f3b73ab1a
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2023-12-25 17:09:32 +0200

    Update install_1.sh

--- install_1.sh
+++ install_1.sh
@@ -11,25 +11,7 @@ sleep 10
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-source ~/.zshrc
 mv Dots/zshrc Dots/.zshrc
 mv Dots/.zshrc ~/.zshrc
 source .zshrc
-clear
-
-clear
-echo "##################################################"
-echo "Successfully set up zsh and with that, the installation has ended. Enjoy your new linux machine!!!  WAIT FOR REBOOT!!!!!!!!!"
-echo "##################################################"
-sleep 5
-echo "Rebooting in 5"
-sleep 1
-echo "Rebooting in 4"
-sleep 1
-echo "Rebooting in 3"
-sleep 1
-echo "Rebooting in 2"
-sleep 1
-echo "Rebooting in 1"
-sleep 1
-reboot
+zsh
