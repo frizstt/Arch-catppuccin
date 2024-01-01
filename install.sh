a9ffbccb01e8c025c32fede00a017bcd1633f560
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2024-01-01 23:19:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "##################################################"
-echo "Welcome. The installation will begin shortly. Be sure to run the post install script once you boot into i3"
-echo "##################################################"
-
 
 ##################################################
 #INSTALL PACKAGES
@@ -12,13 +8,6 @@ sudo pacman -Syy
 sudo pacman -S --noconfirm --needed alacritty btop i3-wm i3lock neofetch neovim picom polybar zathura breeze-gtk firefox flameshot font-manager gnome-weather lxappearance nvidia obsidian pavucontrol polkit-gnome ranger redshift sddm thunar zsh zsh-syntax-highlighting zsh-autosuggestions ttf-font-awesome feh flatpak file-roller discord retroarch libretro-mupen64plus-next libretro-snes9x retroarch-assets-glui steam vlc wget gimp rnote flatpak zathura-pdf-poppler xorg-server xorg-xprop xorg-xrandr rofi tk fcitx5 fcitx5-gtk fcitx5-m17n fcitx5-configtool
 sudo systemctl enable sddm.service
 
-clear
-echo "##################################################"
-echo "Installed all required packages, continuing..."
-echo "##################################################"
-sleep 5
-clear
-
 
 ##################################################
 #APPLY CONFIGURATIONS
@@ -26,12 +15,7 @@ clear
 cp -r Misc/Scripts/ ~/
 cp -r Dots/config/* ~/.config/
 
-clear
-echo "##################################################"
-echo "Applied all required configurations, continuing..."
-echo "##################################################"
-sleep 5
-clear
+
 
 ##################################################
 #INSTALL PACKER.NVIM
@@ -40,12 +24,7 @@ clear
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-clear
-echo "##################################################"
-echo "Installed packer.nvim successfully, continuing..."
-echo "##################################################"
-sleep 5
-clear
+
 
 cp -r Wallpapers/ ~/
 
