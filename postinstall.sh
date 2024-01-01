46c83f2d9ef7f1785511d315076606dfb9d22d3d
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2024-01-01 23:20:12 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -20,7 +20,5 @@ echo "##################################################"
 sleep 5
 clear
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
-zsh
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
