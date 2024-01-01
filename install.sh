c4b0a39751a55706ed648febcc41162f3f73abf3
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2024-01-01 23:50:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,26 @@
 #INSTALL PACKAGES
 ##################################################
 sudo pacman -Syy
-sudo pacman -S --noconfirm --needed alacritty btop i3-wm i3lock neofetch neovim picom polybar zathura breeze-gtk firefox flameshot font-manager gnome-weather lxappearance nvidia obsidian pavucontrol polkit-gnome ranger redshift sddm thunar zsh zsh-syntax-highlighting zsh-autosuggestions ttf-font-awesome feh flatpak file-roller discord retroarch libretro-mupen64plus-next libretro-snes9x retroarch-assets-glui steam vlc wget gimp rnote flatpak zathura-pdf-poppler xorg-server xorg-xprop xorg-xrandr rofi tk fcitx5 fcitx5-gtk fcitx5-m17n fcitx5-configtool man-db
+
+echo "Do you want to download just the required packages(0) or all of them(1)":
+read option
+
+
+if [[ $option == 0 ]]
+
+then
+
+sudo pacman -S --needed alacritty btop i3-wm i3lock neofetch neovim picom polybar breeze-gtk firefox flameshot font-manager lxappearance nvidia obsidian pavucontrol polkit-gnome redshift sddm thunar zsh zsh-syntax-highlighting zsh-autosuggestions ttf-font-awesome feh file-roller wget zathura zathura-pdf-poppler xorg-server xorg-xrandr rofi tk fcitx5 fcitx5-gtk fcitx5-m17n fcitx5-configtool man-db
+
+
+else
+
+sudo pacman -S --needed alacritty btop i3-wm i3lock neofetch neovim picom polybar zathura breeze-gtk firefox flameshot font-manager gnome-weather lxappearance nvidia obsidian pavucontrol polkit-gnome ranger redshift sddm thunar zsh zsh-syntax-highlighting zsh-autosuggestions ttf-font-awesome feh flatpak file-roller discord retroarch libretro-mupen64plus-next libretro-snes9x retroarch-assets-glui steam vlc wget gimp rnote flatpak zathura-pdf-poppler xorg-server xorg-xprop xorg-xrandr rofi tk fcitx5 fcitx5-gtk fcitx5-m17n fcitx5-configtool man-db
+
+
+fi
+
+
 sudo systemctl enable sddm.service
 
 
