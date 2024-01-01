0a94ef614548be51a5febec26717f568057683df
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2024-01-01 23:20:37 +0200

    Update DoNotRunThisManually.sh

--- DoNotRunThisManually.sh
+++ DoNotRunThisManually.sh
@@ -1,12 +1,7 @@
 #!/bin/zsh
 
 ##################################################
-#SET UP ZSH
+#SET UP OH MY ZSH
 ##################################################
-clear
-echo "##################################################"
-echo 'Now, oh-my-zsh will be installed. When it asks you if you want it to be the default shell, say yes. Also after it installs, type "exit" to continue the installation.'
-echo "##################################################"
-sleep 10
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 reboot
