e6262efa334cb42bdc48c04a02bcd898a5671aee
nicktoumpanos <147754899+nicktoumpanos@users.noreply.github.com>
2024-01-09 03:49:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ sudo pacman -S --noconfirm --needed alacritty btop i3-wm i3lock neofetch neovim 
 
 else
 
-sudo pacman -S --noconfirm --needed alacritty btop i3-wm i3lock neofetch neovim picom polybar zathura breeze-gtk firefox flameshot font-manager gnome-weather lxappearance nvidia obsidian pavucontrol polkit-gnome ranger redshift sddm thunar zsh zsh-syntax-highlighting zsh-autosuggestions ttf-font-awesome feh flatpak file-roller discord retroarch libretro-mupen64plus-next libretro-snes9x retroarch-assets-glui steam vlc wget gimp rnote flatpak zathura-pdf-poppler xorg-server xorg-xprop xorg-xrandr rofi tk fcitx5 fcitx5-gtk fcitx5-m17n fcitx5-configtool man-db keepassxc network-manager-applet
+sudo pacman -S --noconfirm --needed alacritty btop i3-wm i3lock neofetch neovim picom polybar zathura breeze-gtk firefox flameshot font-manager gnome-weather lxappearance nvidia obsidian pavucontrol polkit-gnome ranger redshift sddm thunar zsh zsh-syntax-highlighting zsh-autosuggestions ttf-font-awesome feh flatpak file-roller discord retroarch libretro-mupen64plus-next libretro-snes9x retroarch-assets-glui steam vlc wget gimp rnote flatpak zathura-pdf-poppler xorg-server xorg-xprop xorg-xrandr rofi tk fcitx5 fcitx5-gtk fcitx5-m17n fcitx5-configtool man-db keepassxc network-manager-applet kicad
 
 
 fi
